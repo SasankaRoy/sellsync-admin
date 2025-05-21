0cc071b68b8b75cd3812e56644c1681607cdbe44
SasankaRoy <sasankaroy033@gmail.com>
2025-05-21 17:46:54 +0530

    users page done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { AdminDashboard } from "./Pages/admin/AdminDashboard/AdminDashboard";
 import { Sale } from "./Pages/admin/sales/Sale";
 import { Inventory } from "./Pages/admin/Inventory/Inventory";
 import { Reports } from "./Pages/admin/Reports/Reports";
+import { Users } from "./Pages/admin/Users/Users";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/admin/sale" element={<Sale />} exact />
           <Route path="/admin/inventory" element={<Inventory />} exact />
           <Route path="/admin/reports" element={<Reports />} exact />
+          <Route path="/admin/users" element={<Users />} exact />
         </Routes>
       </BrowserRouter>
     </>
--- src/Pages/admin/Users/Users.jsx
+++ src/Pages/admin/Users/Users.jsx
@@ -2,13 +2,16 @@ import React, { useMemo, useState } from "react";
 import { Layout } from "../../../components/common/Layout/Layout";
 import { Overviewcards } from "../../../components/common/Overviewcards/Overviewcards";
 import {
-  CostIcon,
+  BuyPriceIcon,
+  CashierIcon,
   DeleteIcon,
   FilterIcon,
-  ItemSoldIcon,
-  NetsaleAmountIcon,
-  ProfitIcon,
+  InventoryManagerIcon,
+  SellPriceIcon,
   SortIcon,
+  StoreManagerIcon,
+  TotalInventoryIcon,
+  UsersIcon2,
 } from "../../../assets/Svgs/AllSvgs";
 import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
 // Core CSS
@@ -21,91 +24,100 @@ const rowSelection = {
   headerCheckbox: false,
 };
 
-export const Sale = () => {
+export const Users = () => {
   const [rowData, setRowData] = useState([
     {
-      Products: "Lorem ipsum dolor sit consectetur.",
-      Category: "Beer",
-      Customer: "Jack Smith",
-      PaymentStatus: "Complete",
-      Items: "12",
-      Date: "5 April 2025 8:20 PM",
-      Total: "$50",
+      Name: "John Doe",
+      Role: "Admin",
+      PhoneNumber: "1234567890",
+      EmailID: "john@example.com",
+      Status: "Active",
+      LastOnline: "2023-01-01 12:00:00",
+      Actions: "Edit",
     },
     {
-      Products: "Lorem ipsum dolor sit consectetur.",
-      Category: "Beer",
-      Customer: "Jack Smith",
-      PaymentStatus: "Complete",
-      Items: "12",
-      Date: "5 April 2025 8:20 PM",
-      Total: "$50",
+      Name: "Jane Smith",
+      Role: "User",
+      PhoneNumber: "0987654321",
+      EmailID: "jane@example.com",
+      Status: "Active",
+      LastOnline: "2023-01-01 12:00:00",
+      Actions: "Edit",
     },
     {
-      Products: "Lorem ipsum dolor sit consectetur.",
-      Category: "Beer",
-      Customer: "Jack Smith",
-      PaymentStatus: "Complete",
-      Items: "12",
-      Date: "5 April 2025 8:20 PM",
-      Total: "$50",
+      Name: "John Doe",
+      Role: "Admin",
+      PhoneNumber: "1234567890",
+      EmailID: "john@example.com",
+      Status: "Active",
+      LastOnline: "2023-01-01 12:00:00",
+      Actions: "Edit",
     },
     {
-      Products: "Lorem ipsum dolor sit consectetur.",
-      Category: "Beer",
-      Customer: "Jack Smith",
-      PaymentStatus: "Complete",
-      Items: "12",
-      Date: "5 April 2025 8:20 PM",
-      Total: "$50",
+      Name: "John Doe",
+      Role: "Admin",
+      PhoneNumber: "1234567890",
+      EmailID: "john@example.com",
+      Status: "Active",
+      LastOnline: "2023-01-01 12:00:00",
+      Actions: "Edit",
     },
     {
-      Products: "Lorem ipsum dolor sit consectetur.",
-      Category: "Beer",
-      Customer: "Jack Smith",
-      PaymentStatus: "Complete",
-      Items: "12",
-      Date: "5 April 2025 8:20 PM",
-      Total: "$50",
+      Name: "John Doe",
+      Role: "Admin",
+      PhoneNumber: "1234567890",
+      EmailID: "john@example.com",
+      Status: "Active",
+      LastOnline: "2023-01-01 12:00:00",
+      Actions: "Edit",
     },
     {
-      Products: "Lorem ipsum dolor sit consectetur.",
-      Category: "Beer",
-      Customer: "Jack Smith",
-      PaymentStatus: "Complete",
-      Items: "12",
-      Date: "5 April 2025 8:20 PM",
-      Total: "$50",
+      Name: "John Doe",
+      Role: "Admin",
+      PhoneNumber: "1234567890",
+      EmailID: "john@example.com",
+      Status: "Active",
+      LastOnline: "2023-01-01 12:00:00",
+      Actions: "Edit",
     },
     {
-      Products: "Lorem ipsum dolor sit consectetur.",
-      Category: "Beer",
-      Customer: "Jack Smith",
-      PaymentStatus: "Complete",
-      Items: "12",
-      Date: "5 April 2025 8:20 PM",
-      Total: "$50",
+      Name: "John Doe",
+      Role: "Admin",
+      PhoneNumber: "1234567890",
+      EmailID: "john@example.com",
+      Status: "Active",
+      LastOnline: "2023-01-01 12:00:00",
+      Actions: "Edit",
     },
     {
-      Products: "Lorem ipsum dolor sit consectetur.",
-      Category: "Beer",
-      Customer: "Jack Smith",
-      PaymentStatus: "Complete",
-      Items: "12",
-      Date: "5 April 2025 8:20 PM",
-      Total: "$50",
+      Name: "John Doe",
+      Role: "Admin",
+      PhoneNumber: "1234567890",
+      EmailID: "john@example.com",
+      Status: "Active",
+      LastOnline: "2023-01-01 12:00:00",
+      Actions: "Edit",
+    },
+    {
+      Name: "John Doe",
+      Role: "Admin",
+      PhoneNumber: "1234567890",
+      EmailID: "john@example.com",
+      Status: "Active",
+      LastOnline: "2023-01-01 12:00:00",
+      Actions: "Edit",
     },
   ]);
 
   // Column Definitions: Defines & controls grid columns.
   const [colDefs, setColDefs] = useState([
-    { field: "Products" },
-    { field: "Category" },
-    { field: "Customer" },
-    { field: "PaymentStatus" },
-    { field: "Items" },
-    { field: "Date" },
-    { field: "Total" },
+    { field: "Name" },
+    { field: "Role" },
+    { field: "PhoneNumber" },
+    { field: "EmailID" },
+    { field: "Status" },
+    { field: "LastOnline" },
+    { field: "Actions" },
   ]);
 
   // Apply settings across all columns
@@ -117,49 +129,39 @@ export const Sale = () => {
   }, []);
   return (
     <Layout>
-      <div className="pb-14 w-full">
-
-      
+      <div className="w-full gap-5">
         <div className="w-full">
           <div className="flex justify-between items-center">
             <h3 className="text-[1.4dvw] font-semibold text-[var(--mainText-color)]">
-              Sales
+              Users & Roles
             </h3>
-            <select className="px-4 py-1 bg-[var(--button-color2)] text-[var(--primary-color)] rounded-full font-[var(--paraFont)]">
-              <option>Weekly</option>
-              <option>1 week</option>
-              <option>2 week</option>
-              <option>3 week</option>
-              <option>4 week</option>
-            </select>
           </div>
         </div>
 
-
-        <div className="grid grid-cols-4 gap-4 w-full my-6">
+        <div className="grid grid-cols-4 gap-2 w-full my-6">
           <Overviewcards
-            cardTitle="Net Sales Amount"
-            cardValue="$5,456"
-            percent="+4"
-            icon={<NetsaleAmountIcon />}
+            cardTitle="Store Manager"
+            cardValue="2"
+            percent="View"
+            icon={<StoreManagerIcon />}
           />
           <Overviewcards
-            cardTitle="Items Sold"
-            cardValue="59"
-            percent="-2"
-            icon={<ItemSoldIcon />}
+            cardTitle="No.of Cashier"
+            cardValue="8"
+            percent="View"
+            icon={<CashierIcon />}
           />
           <Overviewcards
-            cardTitle="Cost"
-            cardValue="$4,216"
-            percent="-4"
-            icon={<CostIcon />}
+            cardTitle="Inventory Manager"
+            cardValue="4"
+            percent="View"
+            icon={<InventoryManagerIcon />}
           />
           <Overviewcards
-            cardTitle="Profit"
-            cardValue="$1,240"
-            percent="+14"
-            icon={<ProfitIcon />}
+            cardTitle="Registered Uses"
+            cardValue="8,593"
+            percent="View"
+            icon={<UsersIcon2 />}
           />
         </div>
 
@@ -167,9 +169,9 @@ export const Sale = () => {
           <div className="flex justify-between items-center py-1.5 shrink-0">
             <div className="flex justify-center items-center gap-3">
               <select className="font-[500] mainFont px-4 border-none outline-none">
-                <option>All Products</option>
-                <option>All Products</option>
-                <option>All Products</option>
+                <option>All Users</option>
+                <option>All Users</option>
+                <option>All Users</option>
               </select>
               <p className="px-3 text-[1dvw] py-.5 bg-[#F8A61B] rounded-2xl font-[500] border-none text-white">
                 242
