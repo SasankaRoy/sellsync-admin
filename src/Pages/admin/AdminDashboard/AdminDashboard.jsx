7bcc66027134a93ce1932bac18da11b4fccf8a54
atrayee-bhowmik <atrayeebhowmik208@gmail.com>
2025-09-09 21:37:57 +0530

    Employee,Customer,AddProduct

--- src/Pages/admin/AdminDashboard/AdminDashboard.jsx
+++ src/Pages/admin/AdminDashboard/AdminDashboard.jsx
@@ -61,7 +61,7 @@ export const AdminDashboard = () => {
                 <option>3 week</option>
                 <option>4 week</option>
               </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-[var(--primary-color)]">
+              <div className="pointer-events-none   absolute inset-y-0 right-0 flex items-center px-2 text-[var(--primary-color)]">
                 <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                   <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
                 </svg>
