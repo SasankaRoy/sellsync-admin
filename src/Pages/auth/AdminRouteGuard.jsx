b20f037245635977ae487ab1a677e9d7e48851f1
SasankaRoy <sasankaroy033@gmail.com>
2026-02-02 19:00:46 +0530

    update in the auth route layer

--- src/Pages/auth/AdminRouteGuard.jsx
+++ src/Pages/auth/AdminRouteGuard.jsx
@@ -1,22 +1,28 @@
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 import { Loading } from "../../components/UI/Loading/Loading";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 
 const AdminRouteGuard = () => {
   const user = useSelector((state) => state.loggedUser);
+  const [userType, setUserType] = useState(null);
+  useEffect(() => {
+    const userType = Cookies.get("u_type");
+
+    setUserType(userType);
+  }, [user]);
 
   // If user data is not yet loaded, show loading
-  if (!user) {
-    console.log('loading admin route guard',user)
+  if (!userType) {
+    console.log("loading admin route guard", user);
     return <Loading />;
   }
 
   // Check if user role is admin
   // The API returns user_type which can be "admin", "staff", or "employee"
   const isAdmin =
-    user.role === "admin" ||
-    user.userType === "admin" ||
-    user.user_type === "admin";
+    userType === "admin" || userType === "admin" || userType === "admin";
 
   // If user is admin, allow access to admin routes
   // Otherwise, redirect to seller dashboard
